ada916f08e03616f2376682aaa626595131fdf36
Dat Tran <dattran22062005@gmail.com>
2026-04-16 14:19:06 +0700

    Create Manager/dashboard/interviews/reviews/managerContext

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,20 +2,32 @@ interface ButtonProps {
   children: React.ReactNode;
   onClick?: () => void;
   className?: string;
+  variant?: "primary" | "secondary" | "danger";
 }
+const styles = {
+  primary: "bg-blue-600 text-white hover:bg-blue-700",
+  secondary: "bg-gray-200 text-gray-800 hover:bg-gray-300",
+  danger: "bg-red-600 text-white hover:bg-red-700",
+};
 
-const Button = ({ children, onClick, className }: ButtonProps) => {
+const Button = ({
+  children,
+  onClick,
+  className,
+  variant = "primary",
+}: ButtonProps) => {
+  const style = styles[variant];
   return (
     <button
       onClick={onClick}
       className={`
-        px-4 py-2 bg-blue-600 text-white rounded-lg flex items-center justify-center font-semibold hover:bg-blue-800 transition-colors duration-300 cursor-pointer 
-        ${className || ""}
+        px-4 py-2 rounded-lg flex items-center justify-center font-semibold hover:transition-colors duration-300 cursor-pointer 
+
+        ${style} ${className || ""}
       `}
     >
       {children}
     </button>
   );
 };
-
 export default Button;
